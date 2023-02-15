15c62f20b1323361cfe09bd039ba60bb65a16fbc
Arzamasov-Zakhar <arzamasov_z@mail.ru>
2023-02-15 19:50:14 +0300

    Second commit

---
+++ venv/1.6.5.py
@@ -0,0 +1,24 @@
+import math
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+link = "http://suninjuly.github.io/find_link_text"
+
+with webdriver.Chrome() as browser:
+    browser.get(link)
+    link1 = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link1.click()
+
+    input1 = browser.find_element(By.NAME, "first_name")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.NAME, "firstname")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    time.sleep(10)
